e2cfcce56ae2cab122a5b3ae86e41f9d83eabe9a
DiepHoang19 <93962691+DiepHoang19@users.noreply.github.com>
2025-10-16 17:43:27 +0700

    commit update api

--- src/routes/send-mail.js
+++ src/routes/send-mail.js
@@ -1,6 +1,30 @@
 const express = require("express");
 const router = express.Router();
-const { sendMailService } = require("../service/index");
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.MAIL_USER, // your Gmail address
+    pass: process.env.MAIL_PASS, // NOT your Gmail login password
+  },
+});
+
+async function sendMailService({ to, subject, text, html }) {
+  try {
+    await transporter.sendMail({
+      from: process.env.MAIL_FROM, //  gửi từ email nào
+      to,
+      subject,
+      text,
+      html,
+    });
+    console.log("Email sent successfully");
+  } catch (error) {
+    console.log("error", error);
+    throw new Error("Failed to send email");
+  }
+}
 
 // POST /send-mail
 router.post("/", async (req, res) => {
